994585537e880ea52b59082f7de4a745a2c82161
Rômulo Oliveira <romulo.ac.oliveira@gmail.com>
2016-08-17 08:28:53 -0300

    Update setup_aliases.sh
    - Setup global aliases
    - Add `last-tag` and `autosquash` aliases

--- git/setup_aliases.sh
+++ git/setup_aliases.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-git config alias.lasts-commits 'log --oneline -n 5'
-git config alias.ahead-of-master 'log master..HEAD --oneline'
+git config --global alias.lasts-commits 'log --oneline -n 5'
+git config --global alias.ahead-of-master 'log --oneline origin/master..HEAD'
+git config --global alias.last-tag 'describe --abbrev=0 --tags'
+git config --global alias.autosquash 'rebase origin/master --autosquash -i'
