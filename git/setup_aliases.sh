b8cde5e9f832a74432eaf606bade66f9e31d4546
Rômulo Oliveira <romulo.ac.oliveira@gmail.com>
2016-09-16 10:44:48 -0300

    Update setup_aliases.sh
    Add `delete-merged-branches`

--- git/setup_aliases.sh
+++ git/setup_aliases.sh
@@ -4,3 +4,4 @@ git config --global alias.lasts-commits 'log --oneline -n 5'
 git config --global alias.ahead-of-master 'log --oneline origin/master..HEAD'
 git config --global alias.last-tag 'describe --abbrev=0 --tags'
 git config --global alias.autosquash 'rebase origin/master --autosquash -i'
+git config --global alias.delete-merged-branches "branch --merged origin/master | grep -v master | grep -v '*' | xargs git branch -D"
