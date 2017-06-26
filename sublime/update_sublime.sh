2c0db2f84870b016378f1af47248736399c1e3fb
Rômulo Oliveira <romulo.oliveira@luizalabs.com>
2017-06-26 18:29:07 -0300

    Update sublime to 3

--- sublime/update_sublime.sh
+++ sublime/update_sublime.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-SUBLIME_PACKAGES_PATH="$HOME/Library/Application Support/Sublime Text 2/Packages/"
+SUBLIME_PACKAGES_PATH="$HOME/Library/Application Support/Sublime Text 3/Packages/"
 rsync -a -v ./ "$SUBLIME_PACKAGES_PATH" --exclude update_sublime.sh
