32db3fb9bcd7f47db7021dfced127acb38ac890f
Rômulo Oliveira <romulo.ac.oliveira@gmail.com>
2017-06-26 16:52:34 -0300

    Update and rename setup_aliases.sh to .gitconfig
    Remove aliases setup and add the complete .gitconfig

--- git/setup_aliases.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-git config --global alias.lasts-commits 'log --oneline -n 5'
-git config --global alias.ahead-of-master 'log --oneline origin/master..HEAD'
-git config --global alias.last-tag 'describe --abbrev=0 --tags'
-git config --global alias.autosquash 'rebase origin/master --autosquash -i'
-git config --global alias.delete-merged-branches "branch --merged origin/master | grep -v master | grep -v '*' | xargs git branch -D"
