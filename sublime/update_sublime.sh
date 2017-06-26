9699446b3a20631381913823a752c20016d9b52d
Rômulo Oliveira <romulo.oliveira@luizalabs.com>
2017-06-26 17:41:05 -0300

    Move update sublime script to sublime/

--- sublime/update_sublime.sh
+++ sublime/update_sublime.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 SUBLIME_PACKAGES_PATH="$HOME/Library/Application Support/Sublime Text 2/Packages/"
-rsync -a -v sublime/ "$SUBLIME_PACKAGES_PATH"
+rsync -a -v ./ "$SUBLIME_PACKAGES_PATH" --exclude update_sublime.sh
