ac80222e391d1558fe2def61ce1ec94f742a6241
Rômulo Oliveira <romulo.ac.oliveira@gmail.com>
2017-06-26 16:49:03 -0300

    Create config.fish

---
+++ rc_files/.config/fish/config.fish
@@ -0,0 +1,52 @@
+alias s="git status"
+alias d="git diff"
+alias ds="git diff --cached"
+alias yolo="git push -f"
+alias jair="pip list --outdated --format=legacy"
+alias rg="rg --no-heading --color ansi"
+
+# Virtualfish - python virtualenv
+eval (python -m virtualfish)
+
+function my_prompt_pwd -d 'Print the current working directory'
+  echo $PWD | sed -e "s|^$HOME|~|"
+end
+
+# Fish git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+# Git colors
+set __fish_git_prompt_color_branch red
+set __fish_git_prompt_color_dirtystate yellow
+set __fish_git_prompt_color_untrackedfiles cyan
+
+# Status Chars
+set __fish_git_prompt_char_upstream_equal ''
+set __fish_git_prompt_char_untrackedfiles ' ✚ '
+set __fish_git_prompt_char_dirtystate ' ☂ '
+set __fish_git_prompt_char_stagedstate ' → '
+set __fish_git_prompt_char_stashstate ' ↩ '
+set __fish_git_prompt_char_upstream_ahead ' ↑ '
+set __fish_git_prompt_char_upstream_behind ' ↓ '
+
+function fish_prompt
+  set_color $fish_color_cwd
+  # echo ''
+  echo -n (my_prompt_pwd)
+  set_color normal
+  if set -q VIRTUAL_ENV
+    set_color yellow
+    echo -n  " ("(basename "$VIRTUAL_ENV")")"
+    set_color normal
+  end
+  echo (__fish_git_prompt)
+  set_color normal
+  echo ' ⟩ '
+end
+
+set PATH /usr/local/bin /usr/bin /bin /usr/sbin /sbin
+status --is-interactive; and source (pyenv init -|psub)
+
+export LC_ALL=en_US.UTF-8
